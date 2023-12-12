8721fa3e8299867d9bee14472ceef8384b79be75
Kat-cell02 <kathymcallister22@gmail.com>
2023-12-12 11:53:01 -0500

    Fixed probem with html and entered in test data

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState} from 'react';
+import bowling from '../src/bowling.png';
+
+// Configuration parameters
+let myLocalMode = true
+let myClassDate = "20231211";
+let myServerRoot = (myLocalMode)
+    ? "http://localhost:5001"
+    : `https://acsd${myClassDate}.herokuapp.com`;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome ACSD20231211!
-        </p>
-      </header>
-    </div>
-  );
+    const [gameId, setGameId] = useState();
+    const [gameStarted, setGameStarted] = useState(false);
+    const [pins, setPins] = useState();
+    const [ballPins, setBallPins] = useState(0);
+    const [ballRolled, setBallRolled] = useState(false);
+    const [name, setName] = useState("");
+    const [frameNumber, setFrameNumber] = useState(0);
+    const [myPins, setMyPins] = useState(0);
+    const [ballNumber, setBallNumber] = useState(0);
+    const [totalInframe, setTotalInframe] = useState(0);
+
+    const startGame = e => {
+        var myRegisterUrl = `${myServerRoot}/api/mbc/register`;
+        //  alert (myRegisterUrl);
+
+        axios.get(myRegisterUrl).then(response => {
+            setGameId(response.data.id)
+            setGameStarted(true)
+        })
+    }
+
+    const rollOneBall = e => {
+        var myRollUrl = `${myServerRoot}/api/mbc/roll?id=${gameId}`;
+        //  alert (myRollUrl);
+
+        axios.get(myRollUrl).then(response => {
+            console.log(response.data)
+            setPins(response.data.pins)
+            setBallRolled(true)
+            setBallPins(response.data.bullPins)
+            setFrameNumber(response.data.frameNumber)
+            setBallNumber(response.data.ballInFrame)
+            setMyPins(response.data.myPins)
+            setTotalInframe(response.data.totalInframe)
+        })
+    }
+
+    return (
+        <div className="App">
+            <img src={bowling} alt="bowling" width={400}/>
+            <p></p><p></p>
+
+            {!gameStarted && <>
+                <input size={50} onChange={(e) => setName(e.target.value)}
+                       type="text"
+                       placeholder="Enter your Name and start the Party !!"
+                       required
+                />
+                <>&nbsp;</>
+                <button onClick={startGame}>Goooooo!</button>
+            </>
+            }
+
+            {gameStarted && <p>Welcome {name} !</p>}
+            {gameStarted && <p>Your game has unique id which  {gameId}</p>}
+            {gameStarted &&
+                <>
+                    <button onClick={rollOneBall}>Roll Ball</button>
+                    {ballRolled && <p>Frame n°:  {frameNumber}</p>}
+                    {ballRolled && <p>Balle n°:  {ballNumber}</p>}
+                    {ballRolled && <p>You got {myPins} That's a total of {ballPins} pins ! </p>}
+                    {ballRolled && <p>Your total in frame is {totalInframe}</p>}
+                    {ballRolled && <p>Your total Score are {pins}</p>}
+
+                </>
+            }
+        </div>
+    );
 }
 
 export default App;
--- functions/register.js
+++ functions/register.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const registerGame = async (frames=10,pins=10,rolls=2,test="") => {
+const registerGame = async (frames=10,pins=10,rolls=2,test="1234") => {
   //call pinsetter
   let   myUrl = `http://pinsetter.herokuapp.com/pinsetter/?action=register&frames=${frames}&pins=${pins}&rolls=${rolls}&test=${test}`;
   console.log  ("[REQUEST ] " + myUrl)
